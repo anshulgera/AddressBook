bcea06dd7c57317fd0bdb0e22896af5d6767e828
Anshul Gera <anshul.gera@jda.com>
2018-08-02 12:09:50 +0530

    First

---
+++ src/com/jda/test/model/Address.java
@@ -0,0 +1,49 @@
+package com.jda.test.model;
+
+public class Address {
+	private String houseNumber;
+	private String area;
+	private String landmark;
+	private int zipcode;
+	private String city;
+	private String state;
+	public String getHouseNumber() {
+		return houseNumber;
+	}
+	public void setHouseNumber(String houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getLandmark() {
+		return landmark;
+	}
+	public void setLandmark(String landmark) {
+		this.landmark = landmark;
+	}
+	public int getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	
+
+}
---
+++ src/com/jda/test/model/PersonData.java
@@ -0,0 +1,33 @@
+package com.jda.test.model;
+
+public class PersonData {
+	private String firstName;
+	private String lastName;
+	private String number;
+	private Address address;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+}
